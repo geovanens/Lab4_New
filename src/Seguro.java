8bc647bcd1a902a657d7906125ce64f493f0b357
geovanens <geovane.silva@ccc.ufcg.edu.br>
2017-07-23 09:45:30 -0300

    Alterando para classe Abstrata

--- src/Seguro.java
+++ src/Seguro.java
@@ -1,21 +1,16 @@
-import java.math.BigDecimal;
-import java.text.NumberFormat;
-
 /**
  * Classe responsavél por representar um seguro de uma aposta.
  * @author Geovane do Nascimento Silva - 116211149
  *
  */
-public class Seguro {
+public abstract class Seguro {
 	protected int valor;
-	protected String tipo;
 	protected double taxa;
 	
 	/**
 	 * Contrutor para seguro generico, sem valor.
 	 */
 	public Seguro() {
-		this.tipo = "Sem seguro";
 		this.valor = 0;
 	}
 	
@@ -24,7 +19,6 @@ public class Seguro {
 	 * @param valor o valor do seguro
 	 */
 	public Seguro(int valor) {
-		this.tipo = "VALOR";
 		this.valor = valor;
 	}
 	
@@ -34,71 +28,20 @@ public class Seguro {
 	 * @param valorAposta o valor da aposta a qual o seguro pertence.
 	 */
 	public Seguro(double taxa, int valorAposta) {
-		this.tipo = "TAXA";
 		this.taxa = taxa;
 		this.valor = (int) (taxa * valorAposta);
-	}
-
-	/**
-	 * Método auxiliar que transforma a taxa em percentual. 
-	 * @return o valor em porcentagem que a taxa representa.
-	 */
-	private int seguroPercent() {
-	    int percent = (int) (taxa * 100);
-	    return percent;
-	}
-
-	/**
-	 * Método auxiliar para tranformar o valor do seguro em Reais.
-	 * @return o valor em reais de um seguro. 
-	 */
-	private String valorEmReais() {
-		double valorEmReais = this.valor / 100.0;
-		BigDecimal valor = new BigDecimal (valorEmReais);  
-		NumberFormat nf = NumberFormat.getCurrencyInstance();  
-		String formatado = nf.format (valor);
-		return formatado;
 	}
 
 	/**
 	 * Pega o valor do seguro. 
 	 * @return o valor do seguro. 
 	 */
-	public int getValor() {
-		return this.valor;
-	}
+	public abstract int getValor();
 	
-	/**
-	 * Altera o tipo do seguro de taxa para valor e altera seu valor.
-	 * @param valor o valor do seguro. 
-	 */
-	public void setSeguro(int valor) {
-		this.tipo = "VALOR";
-		this.valor = valor;	
-	}
-	
-	/**
-	 * Altera o tipo do seguro de valor para taxa e altera a taxa e o valor. 
-	 * @param taxa a taxa do seguro. 
-	 * @param valorAposta o valor da aposta a qual o seguro pertence. 
-	 */
-	public void setSeguro(double taxa, int valorAposta) {
-		this.tipo = "TAXA";
-		this.taxa = taxa;	
-		this.valor = (int) (taxa * valorAposta);
-	}
 	
 	/**
 	 * Complemtento para o toString da aposta. 
 	 */
 	@Override
-	public String toString() {
-		if(this.tipo.equals("VALOR")) {
-			return " - ASSEGURADA(VALOR) - " + valorEmReais();
-		}
-		else if (this.tipo.equals("TAXA")) {
-			return " - ASSEGURADA (TAXA) - " + seguroPercent() + "%";
-		}
-		return "";
-	}
+	public abstract String toString();
 }
