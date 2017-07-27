e25b083247f01c9f62cff02504a39ea086df0c5d
geovanens <geovane.silva@ccc.ufcg.edu.br>
2017-07-27 06:27:36 -0300

    tentativas de debus

--- src/ComparacaoPorNumeroApostas.java
+++ src/ComparacaoPorNumeroApostas.java
@@ -4,7 +4,16 @@ public class ComparacaoPorNumeroApostas implements Comparator<Cenario> {
 
 	@Override
 	public int compare(Cenario o1, Cenario o2) {
-		return o1.totalApostas() - o2.totalApostas();
+		System.out.println(o1 + " " + o1.totalApostas());
+		System.out.println(o2 + " " + o2.totalApostas());
+		if (o1.totalApostas() - o2.totalApostas() != 0) {
+			System.out.println(o1 + " " + o1.totalApostas());
+			System.out.println(o2 + " " + o2.totalApostas());
+			return o1.totalApostas() - o2.totalApostas();
+		}
+		else {
+			return o1.getId() - o2.getId();
+		}
 	}
 
 }
